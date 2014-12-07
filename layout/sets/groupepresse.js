dfbcf30bc97e2e33a4312dbf2a92bb1873b93e9a
martindaniel4 <martindaniel@192.168.0.21>
2014-12-07 19:24:41 +0100

    add corrections

---
+++ layout/sets/groupepresse.js
@@ -0,0 +1,428 @@
+  var groupes = [
+    {
+      "name":"Groupes de presse",
+      "parent":"null",
+      "children":[
+    {
+    "name": "Hachette",
+    "parent": "Groupes de presse",
+    "children": [
+      {
+        "name": "Amaury",
+        "parent": "Hachette",
+        "part": 25,
+        "children": [
+          {
+            "name": "Le Parisien",
+            "parent": "Amaury"
+          },
+          {
+            "name": "L’Équipe",
+            "parent": "Amaury"
+          },
+          {
+            "name": "France Football",
+            "parent": "Amaury"
+          },
+          {
+            "name": "Tennis magazine",
+            "parent": "Amaury"
+          },
+          {
+            "name": "La dépêche du midi",
+            "parent": "Amaury",
+            "part": 15
+          },
+          {
+            "name": "Psychologies",
+            "parent": "Amaury",
+            "part": 49
+          },
+          {
+            "name": "Zurban",
+            "parent": "Amaury",
+            "part": 75
+          }                         
+        ]
+      },
+      {
+        "name": "Marie-Claire",
+        "parent": "Hachette",
+        "part": 42,
+        "children": [
+          {
+            "name": "Marie-Claire Maison",
+            "parent": "Marie-Claire"
+          },
+          {
+            "name": "Marie-Claire Idée",
+            "parent": "Marie-Claire"
+          },
+          {
+            "name": "Cosmopolitan",
+            "parent": "Marie-Claire"
+          },
+          {
+            "name": "Idées et jardins",
+            "parent": "Marie-Claire"
+          },
+          {
+            "name": "Cuisine et Vins de France",
+            "parent": "Marie-Claire"
+          }                      
+        ]
+      }
+    ]
+   },
+   {
+     "name": "Bayard",
+     "parent": "Groupes de presse",
+     "children": [
+       {
+         "name":"La Croix",
+         "parent":"Bayard"
+       },
+       {
+         "name":"Côté femme",
+         "parent":"Bayard"
+       },
+       {
+         "name":"Pomme d’Api",
+         "parent":"Bayard"
+       },
+       {
+         "name":"J’aime lire",
+         "parent":"Bayard"
+       },
+       {
+         "name":"Astrapi",
+         "parent":"Bayard"
+       },
+       {
+         "name":"Okapi",
+         "parent":"Bayard"
+       },
+       {
+         "name":"Je bouquine",
+         "parent":"Bayard"
+       },
+       {
+         "name":"Le Pélerin magazine",
+         "parent":"Bayard"
+       },
+       {
+         "name":"Phosphore",
+         "parent":"Bayard"
+       },
+       {
+         "name":"Groupe Milan",
+         "parent":"Bayard",
+         "children": [
+           {
+             "name":"Les clés de l’actualité",
+             "parent":"Groupe Milan"
+           },
+           {
+             "name":"Alpes Magazine",
+             "parent":"Groupe Milan"
+           }
+         ] 
+       }
+     ]
+   },
+   {
+     "name": "Dassault",
+     "parent": "Groupes de presse",
+     "children": [
+       {
+         "name":"Le Républicain",
+         "parent":"Dassault"
+       },
+       {
+         "name":"Valeurs actuelles",
+         "parent": "Dassault"
+       },
+       {
+         "name":"Toutes les nouvelles de Versailles",
+         "parent":"Dassault"
+       },
+       {
+         "name":"SOC Presse",
+         "parent":"Dassault",
+         "part": 85,
+         "children":[
+           {
+             "name":"Le Figaro",
+             "parent": "SOC Presse"
+           },
+           {
+             "name":"Le Figaro Magazine",
+             "parent":"SOC Presse"
+           },
+           {
+             "name":"Le JDD",
+             "parent":"SOC Presse",
+             "part": 40
+           },
+           {
+             "name":"Le Dauphiné libéré",
+             "parent":"SOC Presse"
+           },
+           {
+             "name":"Le Progrès",
+             "parent":"SOC Presse"
+           },
+           {
+             "name":"Nice Matin",
+             "parent":"SOC Presse"
+           },
+           {
+             "name":"TV Magazine",
+             "parent":"SOC Presse"
+           },
+           {
+             "name":"L’Express",
+             "parent":"SOC Presse"
+           },
+           {
+             "name":"L’Expansion",
+             "parent":"SOC Presse"
+           },
+           {
+             "name":"L’Etudiant",
+             "parent":"SOC Presse"
+           }
+         ]
+       },
+       {
+         "name":"Rossel",
+         "parent":"Dassault",
+         "children":[
+           {
+             "name":"Le Soir de Bruxelles",
+             "parent":"Rossel"
+           },
+           {
+             "name":"La Voix du Nord",
+             "parent":"Rossel"
+           },
+           {
+             "name":"Nord-Éclair",
+             "parent":"Rossel"
+           }
+         ]
+       }
+     ]
+   },
+   {
+     "name": "Roularta Media Group",
+     "parent": "Groupes de presse",
+     "children": [
+       {
+         "name":"Atmosphères",
+         "parent":"Roularta Media Group"
+       },
+       {
+         "name":"Ideat",
+         "parent":"Roularta Media Group"
+       },
+       {
+         "name":"Maison française",
+         "parent":"Roularta Media Group"
+       },  
+       {
+         "name":"Maison côté sud",
+         "parent":"Roularta Media Group"
+       },  
+       {
+         "name":"Maison côté ouest",
+         "parent":"Roularta Media Group"
+       },  
+       {
+         "name":"A Nous Paris",
+         "parent":"Roularta Media Group"
+       },  
+       {
+         "name":"Maison côté Est",
+         "parent":"Roularta Media Group"
+       }
+     ]
+   },
+   {
+     "name": "La Vie / Le Monde",
+     "parent": "Groupes de presse",
+     "children": [
+       {
+         "name":"Le Monde",
+         "parent": "La Vie / Le Monde"
+       },
+       {
+         "name":"Le Monde de l'Education",
+         "parent":"La Vie / Le Monde"
+       },
+       {
+         "name":"Le Monde Diplomatique",
+         "parent":"La Vie / Le Monde"
+       },
+       {
+         "name":"Le Monde 2",
+         "parent":"La Vie / Le Monde"
+       },
+       {
+         "name":"Manière de voir",
+         "parent":"La Vie / Le Monde"
+       },
+       {
+         "name":"Courrier International",
+         "parent":"La Vie / Le Monde"
+       },
+       {
+         "name":"Les cahiers du cinéma",
+         "parent":"La Vie / Le Monde"
+       },
+       {
+         "name":"Groupe Fleurus Presse",
+         "parent":"La Vie / Le Monde",
+         "children":[
+           {
+             "name":"Papoum",
+             "parent":"La Vie / Le Monde"
+           },
+           {
+             "name":"Abricot",
+             "parent":"La Vie / Le Monde"
+           },
+           {
+             "name":"Je lis déjà",
+             "parent":"La Vie / Le Monde"
+           }
+         ]
+       }
+     ]
+   },
+   {
+     "name": "Prisma",
+     "parent":"Groupes de presse",
+     "children": [
+       {
+         "name":"Cuisine actuelle",
+         "parent":"Prisma"
+       },
+       {
+         "name":"Femme actuelle",
+         "parent":"Prisma"
+       },
+       {
+         "name":"Gala",
+         "parent":"Prisma"
+       },
+       {
+         "name":"Géo",
+         "parent":"Prisma"
+       },
+       {
+         "name":"Télé Loisirs",
+         "parent":"Prisma"
+       },
+       {
+         "name":"Voici",
+         "parent":"Prisma"
+       },
+       {
+         "name":"VSD",
+         "parent":"Prisma"
+       },
+       {
+         "name":"Capital",
+         "parent":"Prisma"
+       },
+       {
+         "name":"Management",
+         "parent":"Prisma"
+       },
+       {
+         "name":"Télé 2 semaines",
+         "parent":"Prisma"
+       },
+       {
+         "name":"National Geographic",
+         "parent":"Prisma",
+         "part": 5
+       }
+     ]
+   },
+   {
+     "name": "SIPA",
+     "parent":"Groupes de presse",
+     "children": [
+       {
+         "name": "Ouest-France",
+         "parent":"SIPA"
+       },
+       {
+         "name": "Presse Océan",
+         "parent":"SIPA"
+       },
+       {
+         "name": "20 Minutes",
+         "parent":"SIPA",
+         "part": 50
+       },
+       {
+         "name": "Le Maine Libre",
+         "parent":"SIPA"
+       },
+     ]
+   },
+   {
+     "name": "EMAP France",
+     "parent":"Groupes de presse",
+     "children": [
+       {
+         "name":"L'Auto Journal",
+         "parent": "EMAP France"
+       },
+       {
+         "name":"30 Millions d'amis",
+         "parent": "EMAP France"
+       },
+       {
+         "name":"Télé-Poche",
+         "parent": "EMAP France"
+       },
+       {
+         "name":"Télé Star",
+         "parent": "EMAP France"
+       },
+       {
+         "name":"Pleine Vie",
+         "parent": "EMAP France"
+       },
+       {
+         "name":"Nous deux",
+         "parent": "EMAP France"
+       },
+       {
+         "name":"Mode et travaux",
+         "parent": "EMAP France"
+       },
+       {
+         "name":"Scienceet Vie",
+         "parent": "EMAP France"
+       },
+       {
+         "name":"Biba",
+         "parent": "EMAP France"
+       },
+       {
+         "name":"20 ans",
+         "parent": "EMAP France"
+       },
+       {
+         "name":"Closer",
+         "parent": "EMAP France"
+       }
+     ]
+   }
+   ]}
+   ];
